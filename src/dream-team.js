3c6677d18045fd5ac2d6ed0a036decd282d61e06
maximzmei <maximzmei@gmail.com>
2020-09-28 21:08:07 +0300

    feat: add new solution for dreamteam

--- src/dream-team.js
+++ src/dream-team.js
@@ -4,9 +4,11 @@ module.exports = function createDreamTeam(members) {
     if (!Array.isArray(members)) return false
     let result = ''
     const sortedArray = members
-        .filter(e => typeof e === 'string')
-        .map(e => {return e.trim()})
-        .sort()
+    .filter(e => typeof e === 'string')
+    .map(e => {return e.trim()})
+    .sort(function (a, b) {
+      return a.localeCompare(b);
+    })
 
     for (const iterator of sortedArray) {
         if (typeof iterator !== 'string') {
@@ -15,5 +17,5 @@ module.exports = function createDreamTeam(members) {
             result += iterator[0]
         }
     }
-    return result.split('').sort().join('').toUpperCase();
+    return result.toUpperCase();
 };
