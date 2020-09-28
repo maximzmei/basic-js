6496535ab52c34446e9fd608ee1f3042b7acaff3
maximzmei <maximzmei@gmail.com>
2020-09-28 18:19:36 +0300

    init: add solution for DreamTeam

--- src/dream-team.js
+++ src/dream-team.js
@@ -1,6 +1,19 @@
 const CustomError = require("../extensions/custom-error");
 
-module.exports = function createDreamTeam(/* members */) {
-  throw new CustomError('Not implemented');
-  // remove line with error and write your code here
+module.exports = function createDreamTeam(members) {
+    if (!Array.isArray(members)) return false
+
+    let result = ''
+    const sortedArray = members.map(function (e) {
+        return e.trim()
+    })
+
+    for (const iterator of sortedArray.sort()) {
+        if (typeof iterator !== 'string') {
+            continue
+        } else {
+            result += iterator[0]
+        }
+    }
+    return result.toUpperCase();
 };
