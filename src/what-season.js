dbebbc557eae69e14b0783feb9deea2811969a9d
maximzmei <maximzmei@gmail.com>
2020-09-29 17:48:51 +0300

    feat: add new solution to what-season

--- src/what-season.js
+++ src/what-season.js
@@ -1,12 +1,15 @@
 const CustomError = require("../extensions/custom-error");
 
 module.exports = function getSeason(date) {
-  if (!date) {return 'Unable to determine the time of year!'}
-  const month = date.getMonth()
-  if (month === 11 || month === 0 || month === 1) { return 'winter' }
-  else if (month >= 2 && month <= 4) { return 'spring' }
-  else if (month >= 5 && month <= 7) { return 'summer' }
-  else { return 'autumn' }
+  if (date === undefined) { return 'Unable to determine the time of year!' }
+  if (Object.prototype.toString.call(date) !== '[object Date]') throw new Error('THROWN')
+  else {
+    const month = date.getMonth()
+    if (month === 11 || month === 0 || month === 1) { return 'winter' }
+    else if (month >= 2 && month <= 4) { return 'spring' }
+    else if (month >= 5 && month <= 7) { return 'summer' }
+    else { return 'autumn' }
+  }
 };
 
 
