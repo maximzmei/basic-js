0209b655b59ae75974a34fb46896477687ef7d7e
maximzmei <maximzmei@gmail.com>
2020-10-09 14:06:48 +0300

    feat: add new solution for transform-array

--- src/transform-array.js
+++ src/transform-array.js
@@ -1,6 +1,23 @@
 const CustomError = require("../extensions/custom-error");
 
-module.exports = function transform(/* arr */) {
-  throw new CustomError('Not implemented');
-  // remove line with error and write your code here
-};
+module.exports = function transform(arr) {
+  if (!Array.isArray(arr)) throw new Error
+  let newArray = []
+  for (let i = 0; i < arr.length; i++) {
+    if (arr[i] === '--discard-next') {
+      newArray.push(arr[i + 1])
+      newArray.pop()
+      i += 2
+    } else if (arr[i] === '--discard-prev') {
+      newArray.pop()
+    } else if (arr[i] === '--double-next') {
+      newArray.push(arr[i + 1])
+    } else if (arr[i] === '--double-prev') {
+      newArray.push(arr[i - 1])
+    } else {
+      newArray.push(arr[i])
+    }
+  }
+  if (arr.includes('undefined')) {return newArray}
+  else {return newArray.filter((el) => el !== undefined)} 
+}
