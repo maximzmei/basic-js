8e8e35364df1f5f8d9aa086bd5d65c777510e320
Maksim Rynkov <maximzmei@gmail.com>
2020-10-15 21:51:02 +0300

    feat: add new solution for extended-repeater

--- src/extended-repeater.js
+++ src/extended-repeater.js
@@ -1,7 +1,26 @@
 const CustomError = require("../extensions/custom-error");
 
-module.exports = function repeater(/* str, options */) {
-  throw new CustomError('Not implemented');
-  // remove line with error and write your code here
-};
+module.exports = function repeater(str, options) {
+  let result = []
+  let additionString = ''
+  if (str === null) {str = 'null'}
+  str = str.toString()
+  if (options.addition === null) {options.addition = 'null'}
+  if (options.addition === false) {options.addition = 'false'}
+  if (!options.addition) { options.addition = '' }
+  else { options.addition.toString() }
   
+  if (!options.repeatTimes) {options.repeatTimes = 1}
+  if (!options.additionRepeatTimes) {options.additionRepeatTimes = 1}
+  
+  for (let index = 0; index < options.additionRepeatTimes; index++) {
+    additionString += `${options.addition}${index+1!=options.additionRepeatTimes?options.additionSeparator:''}`
+  }
+
+  for (let index = 0; index < options.repeatTimes; index++) {
+    result.push(`${str}${additionString}`)
+  }
+  if (!options.separator) {options.separator = '+'}
+  if (!options.additionSeparator) {options.additionSeparator = '|'}
+  return result.join(options.separator)
+}
