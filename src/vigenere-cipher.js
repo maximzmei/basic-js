fffb923f1ca486c5bc65cea3d3e1a75d4ac8f223
maximzmei <maximzmei@gmail.com>
2020-10-21 23:45:06 +0300

    feat: add solution for last task

--- src/vigenere-cipher.js
+++ src/vigenere-cipher.js
@@ -1,13 +1,30 @@
-const CustomError = require("../extensions/custom-error");
-
 class VigenereCipheringMachine {
-  encrypt() {
-    throw new CustomError('Not implemented');
-    // remove line with error and write your code here
-  }    
-  decrypt() {
-    throw new CustomError('Not implemented');
-    // remove line with error and write your code here
+  constructor(rev = true) {
+    this.rev = !rev;
+  }
+
+  _vigenere(message, key, decode = false) {
+    if (!message || !key) throw new Error();
+
+    let i = 0, b;
+    key = key.toUpperCase().replace(/[^A-Z]/g, '');
+
+    const ordA = a => a.charCodeAt(0) - 65;
+
+    const result = message.toUpperCase().replace(/[A-Z]/g, a => {
+      b = key[i++ % key.length];
+      return String.fromCharCode(((ordA(a) + (decode ? 26 - ordA(b) : ordA(b))) % 26 + 65));
+    });
+
+    return this.rev? result.split('').reverse().join('') : result;
+  }
+
+  encrypt(message, key) {
+    return this._vigenere(message, key)
+  }
+
+  decrypt(message, key) {
+    return this._vigenere(message, key, true)
   }
 }
 
